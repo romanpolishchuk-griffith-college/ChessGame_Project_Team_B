c2e65b044aabe62d982b5ffa9379cd863e14b089
Roman Polishchuk <romanpolishchuk2006@gmail.com>
2025-03-12 11:02:57 +0000

    Simple window creating

--- src/griffith/Main.java
+++ src/griffith/Main.java
@@ -1,5 +1,12 @@
 package griffith;
 
+import javax.swing.*;
+
 public class Main {
-	
+	public static void main(String[] args) {
+		JFrame window = new JFrame("ChessDreamApp");
+		window.setTitle("ChessDream");
+		window.setSize(400, 400);
+		window.setVisible(true);
+	}
 }
--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module ChessDream {
+	requires java.desktop;
 }
