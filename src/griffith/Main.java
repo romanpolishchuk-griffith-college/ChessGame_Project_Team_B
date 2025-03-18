5caede815e84bba1dab9d346ccef242668c71296
Roman Polishchuk <romanpolishchuk2006@gmail.com>
2025-03-18 15:13:08 +0000

    Add setup method in Renderer

--- src/griffith/Main.java
+++ src/griffith/Main.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 
 public class Main {
 	public static void main(String[] args) {
-		
+		Game game = new Game("Super Chess", 1280, 720);
+		game.Run();
 	}
 }
--- src/griffith/Renderer.java
+++ src/griffith/Renderer.java
@@ -3,10 +3,17 @@ package griffith;
 import javax.swing.JFrame;
 
 public class Renderer {
-	public void Render() {
-		JFrame window = new JFrame("ChessDreamApp");
-		window.setTitle("ChessDream");
-		window.setSize(400, 400);
+	
+	private JFrame window;
+	
+	public void Setup(String title, int width, int heigth) {
+		window = new JFrame(title);
+		window.setTitle(title);
+		window.setSize(width, heigth);
 		window.setVisible(true);
+	}
+	
+	public void Render() {
+
 	}
 }
