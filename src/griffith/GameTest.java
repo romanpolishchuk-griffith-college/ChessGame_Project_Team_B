6cf735a32d71c21ce1e4c53b4606ea5847ebcda9
Roman Polishchuk <romanpolishchuk2006@gmail.com>
2025-03-18 14:52:37 +0000

    Implement testGameInitProperties in the GameTest

--- src/griffith/GameTest.java
+++ src/griffith/GameTest.java
@@ -8,7 +8,28 @@ class GameTest {
 
 	@Test
 	void testGameInitProperties() {
+		Game game1 = new Game("Title1", 100, 200);
+		assertEquals(100, game1.getWindowWidth());
+		assertEquals(200, game1.getWindowHeight());
+		assertEquals("Title1", game1.getWindowTitle());
 		
+		Game game2 = new Game("Title2", -200, -100);
+		//Default resolution if width and height is incorrect
+		assertEquals(1000, game2.getWindowWidth());
+		assertEquals(1000, game2.getWindowHeight());
+		assertEquals("Title1", game2.getWindowTitle());
+		
+		Game game3 = new Game(null, 100, 200);
+		assertEquals(100, game3.getWindowWidth());
+		assertEquals(200, game3.getWindowHeight());
+		//Default title if title is null or empty
+		assertEquals("Title", game3.getWindowTitle());
+		
+		Game game4 = new Game("", 100, 200);
+		assertEquals(100, game4.getWindowWidth());
+		assertEquals(200, game4.getWindowHeight());
+		//Default title if title is null or empty
+		assertEquals("Title", game4.getWindowTitle());
 	}
 
 }
