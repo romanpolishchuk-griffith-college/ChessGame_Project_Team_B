944304e635b2304921e10c05970f44fe2ec9a3d7
Stanislav Kril <kril.stas22@gmail.com>
2025-03-21 20:22:39 +0000

    Implementation <Board.java, Renderer.java> (Drawing chess board in window)

--- src/griffith/Board.java
+++ src/griffith/Board.java
@@ -3,55 +3,71 @@ package griffith;
 import java.awt.Color;
 import java.awt.Graphics;
 
-public class Board  {
-	private static final int BOARD_SIZE = 8;
+import javax.swing.JPanel;
+
+public class Board extends JPanel  {
+  private static final int BOARD_SIZE = 8;
     private static final int SQUARE_SIZE = 70;
     
- 
+    public Board() {
+        setPreferredSize(new java.awt.Dimension(BOARD_SIZE * SQUARE_SIZE, BOARD_SIZE * SQUARE_SIZE));
+    }
     
-	private ChessPiece[][] board = {
-			null, null, null, null, null, null, null, null,
-			null, null, null, null, null, null, null, null,
-			null, null, null, null, null, null, null, null,
-			null, null, null, null, null, null, null, null,
-			null, null, null, null, null, null, null, null,
-			null, null, null, null, null, null, null, null,
-			null, null, null, null, null, null, null, null,
-			null, null, null, null, null, null, null, null
-	};
-	
+  private ChessPiece[][] board = {
+      null, null, null, null, null, null, null, null,
+      null, null, null, null, null, null, null, null,
+      null, null, null, null, null, null, null, null,
+      null, null, null, null, null, null, null, null,
+      null, null, null, null, null, null, null, null,
+      null, null, null, null, null, null, null, null,
+      null, null, null, null, null, null, null, null,
+      null, null, null, null, null, null, null, null
+  };
+  
 
-	
-	public int getBoredSize(){
-		return BOARD_SIZE;
-	}
+  
+  public int getBoredSize(){
+    return BOARD_SIZE;
+  }
 
-	public ChessPiece setPiece() {
-		//Stab
-		return null;
-	}
-	
-	public ChessPiece getPiece() {
-		//Stab
-		return null;
-	}
-	
-	 public void draw(Graphics g) {
-	       
-	       
-	    }
-	 
-	public Color  getSquareColor(int row, int column) {
-		
-		if ((row + column) % 2 == 0) {
+  public ChessPiece setPiece() {
+    //Stab
+    return null;
+  }
+  
+  public ChessPiece getPiece() {
+    //Stab
+    return null;
+  }
+  
+   public void draw(Graphics g) {
+          // Draw the chess board squares
+          for (int row = 0; row < BOARD_SIZE; row++) {
+              for (int col = 0; col < BOARD_SIZE; col++) {
+                  // Set color based on position
+                  g.setColor(getSquareColor(row, col));;
+                  
+                  // Draw the square
+                  g.fillRect(col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
+              }
+          }
+         
+      }
+  
+   @Override
+  protected void paintComponent(Graphics g) {
+          super.paintComponent(g);
+          draw(g);
+      }
+   
+  public Color  getSquareColor(int row, int column) {
+ 
+    if ((row + column) % 2 == 0) {
             return Color.WHITE;
         } else {
             return Color.BLACK;
         }
-		
-	}
-	
+    
+  }
+  
 }
-
-
-
--- src/griffith/Renderer.java
+++ src/griffith/Renderer.java
@@ -5,15 +5,21 @@ import javax.swing.JFrame;
 public class Renderer {
 	
 	private JFrame window;
+	private Board board;
 	
 	public void Setup(String title, int width, int heigth) {
 		window = new JFrame(title);
 		window.setTitle(title);
 		window.setSize(width, heigth);
 		window.setVisible(true);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		board = new Board();
+		window.add(board);
+		window.setVisible(true);
 	}
 	
 	public void Render() {
-
+		board.repaint();
 	}
 }
