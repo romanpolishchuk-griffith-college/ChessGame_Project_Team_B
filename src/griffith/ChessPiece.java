14cf11bdc4686810248be1f94def1c9664cdbc59
Stanislav Kril <kril.stas22@gmail.com>
2025-03-21 18:34:47 +0000

    Update <ChessPiece.java> (method arguments)

--- src/griffith/ChessPiece.java
+++ src/griffith/ChessPiece.java
@@ -1,11 +1,12 @@
 package griffith;
 
+import java.awt.Graphics;
 public abstract class ChessPiece {
 
 	abstract public String getValidMoves();
 	
 	abstract public boolean isMoveValid();
 	
-	abstract public void draw();
+	abstract public void draw(Graphics g, int x, int y);
 	
 }
