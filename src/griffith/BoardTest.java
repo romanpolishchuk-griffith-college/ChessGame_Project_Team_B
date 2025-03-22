66958019e4a4b9d47a01628378ff9d7f615970fc
Roman Polishchuk <romanpolishchuk2006@gmail.com>
2025-03-22 03:11:53 +0000

    Create BoardTest test class

---
+++ src/griffith/BoardTest.java
@@ -0,0 +1,19 @@
+package griffith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BoardTest {
+
+	@Test
+	void getPieceTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void setPieceTest() {
+		fail("Not yet implemented");
+	}
+
+}
