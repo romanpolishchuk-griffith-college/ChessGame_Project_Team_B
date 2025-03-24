a5371c794f349d2a736ee50301c43dd6a331af02
Roman Polishchuk <romanpolishchuk2006@gmail.com>
2025-03-24 11:32:16 +0000

    Add ChessPiece constructor

--- src/griffith/ChessPiece.java
+++ src/griffith/ChessPiece.java
@@ -2,6 +2,12 @@ package griffith;
 
 public abstract class ChessPiece {
 
+	private	Board board;
+
+	ChessPiece(Board board) {
+		this.board = board;
+	}
+
 	abstract public String getValidMoves();
 	
 	abstract public boolean isMoveValid();
