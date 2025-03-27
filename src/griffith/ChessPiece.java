da868d5f000ecec0b7d94b9b1770ca77d9da77a2
Stanislav Kril <kril.stas22@gmail.com>
2025-03-27 21:18:40 +0000

    Update <ChessPiece.java, Knight.java> (structure of class)

--- src/griffith/ChessPiece.java
+++ src/griffith/ChessPiece.java
@@ -2,11 +2,22 @@ package griffith;
 
 import java.awt.Graphics;
 public abstract class ChessPiece {
+	
+	 protected int col, row;
+	 protected int xPos, yPos;
+	 protected boolean isWhite;
+	 protected String name;
+	 protected Board board;
 
+	 public ChessPiece(Board board) {
+	        this.board = board;
+	    }
+
+	    
 	abstract public String getValidMoves();
 	
 	abstract public boolean isMoveValid();
 	
-	abstract public void draw(Graphics g, int x, int y);
+	abstract public void draw();
 	
 }
--- src/griffith/Knight.java
+++ src/griffith/Knight.java
@@ -2,4 +2,31 @@ package griffith;
 
 public class Knight extends ChessPiece {
 
+	public Knight(Board board, int col, int row, boolean isWhite) {
+        super(board);
+        this.col = col;
+        this.row = row;
+        this.isWhite = isWhite;
+        this.name = "Knight";
+   
+    }
+	
+	@Override
+	public String getValidMoves() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isMoveValid() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
