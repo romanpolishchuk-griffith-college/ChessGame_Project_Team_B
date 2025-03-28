c7cca380d38df22c49213c4c92abb20c5479be39
Roman Polishchuk <romanpolishchuk2006@gmail.com>
2025-03-28 10:31:56 +0000

    Implement getValidMoves in Pawn

--- src/griffith/Pawn.java
+++ src/griffith/Pawn.java
@@ -7,8 +7,33 @@ public class Pawn extends ChessPiece {
 
 	@Override
 	public String getValidMoves() {
-		// TODO Auto-generated method stub
-		return null;
+		String moves = "";
+        int pawnX = -1;
+        int pawnY = -1;
+
+        for (int y = 0; y < board.getBoard().length; y++) {
+            for (int x = 0; x < board.getBoard()[y].length; x++) {
+                if (board.getPiece(x, y) == this) {
+                	pawnX = x;
+                	pawnY = y;
+                    break;
+                }
+            }
+        }
+        
+        if (board.getPiece(pawnX, pawnY + 1) == null) {
+            moves += pawnX + "," + (pawnY + 1) + " ";
+        }
+        
+        if (board.getPiece(pawnX + 1, pawnY + 1) != null) {
+            moves += (pawnX + 1) + "," + (pawnY + 1) + " ";
+        }
+        
+        if (board.getPiece(pawnX - 1, pawnY + 1) != null) {
+            moves += (pawnX - 1) + "," + (pawnY + 1) + " ";
+        }
+        
+        return moves.trim();
 	}
 
 	@Override
