4fe2e3c10e5e9a630c3e5f3b58e7061cfa7890d8
Stanislav Kril <kril.stas22@gmail.com>
2025-03-28 21:14:31 +0000

    Implementation <King.java> ( fields and consturctor to pass tests )

--- src/griffith/King.java
+++ src/griffith/King.java
@@ -2,4 +2,29 @@ package griffith;
 
 public class King extends ChessPiece {
 
+	public King(Board board, int col, int row, boolean isWhite) {
+		 super(board);
+	        this.col = col;
+	        this.row = row;
+	        this.isWhite = isWhite;
+	        this.name = "King";
+	        
+	        // Load the appropriate knight image
+	        String imageName = isWhite ? "w - king.png" : "b - king.png";
+	        loadImage(imageName);
+	   
+	}
+
+	@Override
+	public String getValidMoves() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isMoveValid() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
