a21a3a9484273cfab9ce4a6aedbf56e5bd0cf6d7
Stanislav Kril <kril.stas22@gmail.com>
2025-03-28 21:40:15 +0000

    Implementation <Pawn.java> ( fields and consturctor to pass tests )

--- src/griffith/Pawn.java
+++ src/griffith/Pawn.java
@@ -2,4 +2,29 @@ package griffith;
 
 public class Pawn extends ChessPiece {
 
+	public Pawn(Board board, int col, int row, boolean isWhite) {
+		 super(board);
+	        this.col = col;
+	        this.row = row;
+	        this.isWhite = isWhite;
+	        this.name = "Pawn";
+	        
+	        // Load the appropriate knight image
+	        String imageName = isWhite ? "w - pawn.png" : "b - pawn.png";
+	        loadImage(imageName);
+	   
+	}
+
+	@Override
+	public String getValidMoves() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isMoveValid() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
