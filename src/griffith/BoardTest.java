b164fd8ad979aea50ba7ed44ed29fe478acc63f0
Stanislav Kril <kril.stas22@gmail.com>
2025-03-28 19:49:08 +0000

    Update <Board.java> ( Remove reudant tests )

--- src/griffith/BoardTest.java
+++ src/griffith/BoardTest.java
@@ -9,15 +9,16 @@ import org.junit.jupiter.api.Test;
 
 public class BoardTest {
 
-	private Board board;
-	private Knight whiteKnight;
-	private Knight blackKnight;
+	private Board board; 
+	 
+	Knight whiteKnight;
+	Knight blackKnight;
+	Rook whiteRook;
+	Rook blackRook;
 	
 	@BeforeEach
 	public void setUp() {
 		board = new Board();
-		whiteKnight = new Knight(board, 1, 0, true);  // b1 position
-	    blackKnight = new Knight(board, 6, 7, false); // g8 position
 	}
 	
 	@Test
@@ -34,40 +35,31 @@ public class BoardTest {
 	    assertEquals(Color.BLACK, board.getSquareColor(1, 0));
 	    assertEquals(Color.WHITE, board.getSquareColor(1, 1));
 	}
-	
-	@Test
-	  void testKnightMovementValidation() {
-		// TODO
-	    // Test that isMoveValid() returns false (since it's not implemented yet)
-	    assertFalse(whiteKnight.isMoveValid());
-	    assertFalse(blackKnight.isMoveValid());
-	  }
-
-	  @Test
-	  void testKnightValidMoves() {
-		// TODO
-	    // Test that getValidMoves() returns empty string (since it's not implemented yet)
-	    assertEquals("", whiteKnight.getValidMoves());
-	    assertEquals("", blackKnight.getValidMoves());
-	  }
 	  
 	  @Test
 	  void testKnightInitialization() {
+		  
+		Knight whiteKnight = new Knight(board, 1, 0, true);
+		Knight blackKnight = new Knight(board, 6, 7, false);
+
 	    // Test white knight initialization
-	    assertEquals(1, whiteKnight.col);
-	    assertEquals(0, whiteKnight.row);
-	    assertTrue(whiteKnight.isWhite);
-	    assertEquals("Knight", whiteKnight.name);
+	    assertEquals(1, whiteKnight.col);		 	// Test position
+	    assertEquals(0, whiteKnight.row); 			// Test position
+	    assertTrue(whiteKnight.isWhite); 			// Test color
+	    assertEquals("Knight", whiteKnight.name); 	// Test name
 
 	    // Test black knight initialization
-	    assertEquals(6, blackKnight.col);
-	    assertEquals(7, blackKnight.row);
-	    assertFalse(blackKnight.isWhite);
-	    assertEquals("Knight", blackKnight.name);
+	    assertEquals(6, blackKnight.col); 			// Test position
+	    assertEquals(7, blackKnight.row);			// Test position
+	    assertFalse(blackKnight.isWhite); 			// Test color
+	    assertEquals("Knight", blackKnight.name); 	// Test name
 	  }
 	  
 	  @Test
 	  void testKnightPositionCalculation() {
+		  
+		Knight whiteKnight = new Knight(board, 1, 0, true);
+		Knight blackKnight = new Knight(board, 6, 7, false);
 	    // Test white knight position calculation
 	    whiteKnight.updatePosition();
 	    assertEquals(80, whiteKnight.xPos);  // 1 * SQUARE_SIZE
@@ -78,11 +70,6 @@ public class BoardTest {
 	    assertEquals(480, blackKnight.xPos); // 6 * SQUARE_SIZE
 	    assertEquals(560, blackKnight.yPos); // 7 * SQUARE_SIZE
 	  }
-	 
-	  @Test
-	  void testKnightImageLoading() {
-	    // Test that sprites are loaded (not null)
-	    assertNotNull(whiteKnight.sprite);
-	    assertNotNull(blackKnight.sprite);
-	  }
+
+
 }
