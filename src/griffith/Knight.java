5fdeaab9f7f5ef891761c4bbfa5d43ab7f9ff730
Roman Polishchuk <romanpolishchuk2006@gmail.com>
2025-03-28 11:15:31 +0000

    Implement getValidMoves in Knight

--- src/griffith/Knight.java
+++ src/griffith/Knight.java
@@ -7,8 +7,61 @@ public class Knight extends ChessPiece {
 
 	@Override
 	public String getValidMoves() {
-		// TODO Auto-generated method stub
-		return null;
+		String moves = "";
+        int knightX = -1;
+        int knightY = -1;
+
+        for (int y = 0; y < board.getBoard().length; y++) {
+            for (int x = 0; x < board.getBoard()[y].length; x++) {
+                if (board.getPiece(x, y) == this) {
+                	knightX = x;
+                	knightY = y;
+                    break;
+                }
+            }
+        }
+        
+        if (knightX - 2 < board.getBoard()[0].length && knightX - 2 >= 0
+        		&& knightY + 1 < board.getBoard().length && knightY + 1 >= 0) {
+            moves += (knightX - 2) + "," + (knightY + 1) + " ";
+        }
+        
+        if (knightX - 1 < board.getBoard()[0].length && knightX - 1 >= 0
+        		&& knightY + 2 < board.getBoard().length && knightY + 2 >= 0) {
+            moves += (knightX - 1) + "," + (knightY + 1) + " ";
+        }
+        
+        if (knightX + 1 < board.getBoard()[0].length && knightX + 1 >= 0
+        		&& knightY + 2 < board.getBoard().length && knightY + 2 >= 0) {
+            moves += (knightX + 1) + "," + (knightY + 2) + " ";
+        }
+        
+        if (knightX + 2 < board.getBoard()[0].length && knightX + 2 >= 0
+        		&& knightY + 1 < board.getBoard().length && knightY + 1 >= 0) {
+            moves += (knightX + 2) + "," + (knightY + 1) + " ";
+        }
+        
+        if (knightX + 2 < board.getBoard()[0].length && knightX + 2 >= 0
+        		&& knightY - 1 < board.getBoard().length && knightY - 1 >= 0) {
+            moves += (knightX + 2) + "," + (knightY - 1) + " ";
+        }
+        
+        if (knightX + 1 < board.getBoard()[0].length && knightX + 1 >= 0
+        		&& knightY - 2 < board.getBoard().length && knightY - 2 >= 0) {
+            moves += (knightX + 1) + "," + (knightY - 2) + " ";
+        }
+        
+        if (knightX - 1 < board.getBoard()[0].length && knightX - 1 >= 0
+        		&& knightY - 2 < board.getBoard().length && knightY - 2 >= 0) {
+            moves += (knightX - 1) + "," + (knightY - 2) + " ";
+        }
+        
+        if (knightX - 2 < board.getBoard()[0].length && knightX - 2 >= 0
+        		&& knightY - 1 < board.getBoard().length && knightY - 1 >= 0) {
+            moves += (knightX - 2) + "," + (knightY - 1) + " ";
+        }
+        
+        return moves.trim();
 	}
 
 	@Override
