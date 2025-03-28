8b6bb56472b68fc79587b3ef55eada90a08de22d
Stanislav Kril <kril.stas22@gmail.com>
2025-03-28 20:16:45 +0000

    Implementation <Rook.java> ( fields and consturctor to pass tests )

--- src/griffith/Rook.java
+++ src/griffith/Rook.java
@@ -2,4 +2,29 @@ package griffith;
 
 public class Rook extends ChessPiece {
 
+	public Rook(Board board, int col, int row, boolean isWhite) {
+		 super(board);
+	        this.col = col;
+	        this.row = row;
+	        this.isWhite = isWhite;
+	        this.name = "Rook";
+	        
+	        // Load the appropriate knight image
+	        String imageName = isWhite ? "w - rook.png" : "b - rook.png";
+	        loadImage(imageName);
+	   
+	}
+
+	@Override
+	public String getValidMoves() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isMoveValid() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
