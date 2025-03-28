86b8e9ae527685b413893e95cc3e72ba55e2bdd6
Stanislav Kril <kril.stas22@gmail.com>
2025-03-28 22:01:45 +0000

    Update <Board.java> ( initializePieces() method to draw other chessPieces  )

--- src/griffith/Board.java
+++ src/griffith/Board.java
@@ -17,11 +17,43 @@ public class Board extends JPanel  {
     }
     
     private void initializePieces() {
-        // Initialize knights
-        board[0][1] = new Knight(this, 1, 0, true);  // White knight
-        board[0][6] = new Knight(this, 6, 0, true);  // White knight
-        board[7][1] = new Knight(this, 1, 7, false); // Black knight
-        board[7][6] = new Knight(this, 6, 7, false); // Black knight
+        // Initialize White pieces
+        // Pawns
+        for (int col = 0; col < BOARD_SIZE; col++) {
+          board[1][col] = new Pawn(this, col, 1, true);
+      }
+      // Rooks
+      board[0][0] = new Rook(this, 0, 0, true);
+      board[0][7] = new Rook(this, 7, 0, true);
+      // Knights
+      board[0][1] = new Knight(this, 1, 0, true);
+      board[0][6] = new Knight(this, 6, 0, true);
+      // Bishops
+      board[0][2] = new Bishop(this, 2, 0, true);
+      board[0][5] = new Bishop(this, 5, 0, true);
+      // Queen
+      board[0][3] = new Queen(this, 3, 0, true);
+      // King
+      board[0][4] = new King(this, 4, 0, true);
+
+      // Initialize Black pieces
+      // Pawns
+      for (int col = 0; col < BOARD_SIZE; col++) {
+          board[6][col] = new Pawn(this, col, 6, false);
+      }
+      // Rooks
+      board[7][0] = new Rook(this, 0, 7, false);
+      board[7][7] = new Rook(this, 7, 7, false);
+      // Knights
+      board[7][1] = new Knight(this, 1, 7, false);
+      board[7][6] = new Knight(this, 6, 7, false);
+      // Bishops
+      board[7][2] = new Bishop(this, 2, 7, false);
+      board[7][5] = new Bishop(this, 5, 7, false);
+      // Queen
+      board[7][3] = new Queen(this, 3, 7, false);
+      // King
+      board[7][4] = new King(this, 4, 7, false);
     }
     
 //  private ChessPiece[][] board = {
