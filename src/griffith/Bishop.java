20238c8d93e701df4f2aabfa6ecb7be53eeb82e4
Stanislav Kril <kril.stas22@gmail.com>
2025-03-28 20:36:03 +0000

    Implementation <Bishop.java> ( fields and consturctor to pass tests )

--- src/griffith/Bishop.java
+++ src/griffith/Bishop.java
@@ -2,4 +2,29 @@ package griffith;
 
 public class Bishop extends ChessPiece {
 
+	public Bishop(Board board, int col, int row, boolean isWhite) {
+		 super(board);
+	        this.col = col;
+	        this.row = row;
+	        this.isWhite = isWhite;
+	        this.name = "Bishop";
+	        
+	        // Load the appropriate knight image
+	        String imageName = isWhite ? "w - bishop.png" : "b - bishop.png";
+	        loadImage(imageName);
+	   
+	}
+
+	@Override
+	public String getValidMoves() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isMoveValid() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
