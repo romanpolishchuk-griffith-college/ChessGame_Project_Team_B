5ce0b86a59d826ef28b86ab76de343380ef9f6e4
Mythmaster1x <arshsharma2404@gmail.com>
2025-03-30 02:09:20 +0100

    imported AWT, and added necessary UI components

--- src/griffith/ChessGameUI.java
+++ src/griffith/ChessGameUI.java
@@ -4,5 +4,10 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ChessGameUI {
+	    private JFrame frame;  //main window of the game
+	    private JPanel boardPanel, menuPanel, capturedPanel, statsPanel, welcomePanel; //different panels for different sections
+	    private JLabel statusLabel, moveCounterLabel; //game information
+	    private int moveCounter = 0;  //cOunts the number of moves made in the game
+
    
 }
