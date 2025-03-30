5bb0139d6a90b78904489b1c374995dbd16fa0a8
Mythmaster1x <arshsharma2404@gmail.com>
2025-03-30 02:32:59 +0100

    setting up the initializeUI method adding a Main window for the game and basic layout

--- src/griffith/ChessGameUI.java
+++ src/griffith/ChessGameUI.java
@@ -13,6 +13,9 @@ public class ChessGameUI {
 	        initializeUI();//Initializes the graphical interface 
 	    }
 	    private void initializeUI() {
-	       
+	    	 frame = new JFrame("Chess");//JFrame with title "Chess"
+	         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Exits the application when window is closed
+	         frame.setSize(800, 600);//Window dimensions
+	         frame.setLayout(new BorderLayout());//Using BorderLayout to organize panels
 	    }
 }
