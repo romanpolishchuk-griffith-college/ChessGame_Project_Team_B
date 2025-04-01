381979b319199073e824018824a2516dbf745ce0
Radu Lupu <raduconstantin.lupu@student.griffith.ie>
2025-04-01 23:22:08 +0100

    Created a base method that handles the way the computer plays the game.

--- src/griffith/Multiplayer.java
+++ src/griffith/Multiplayer.java
@@ -22,4 +22,18 @@ public class Multiplayer  {
     }
 
    
+    private void handleComputerMove() {
+        // Computer makes a move
+        int[] move = gameLogic.getComputerMove(chessBoard); // Get a move from the computer
+        if (move != null) {
+            gameLogic.executeMove(chessBoard, move[0], move[1], move[2], move[3]); // Execute the move
+            chessBoard.repaint(); // Update the board
+            isWhiteTurn = true; // Switch back to player's turn
+
+            // Check if the game is over
+            if (gameLogic.isGameOver()) {
+                JOptionPane.showMessageDialog(null, "Game Over! Black (Computer) wins!");
+            }
+        }
+    }
 }
