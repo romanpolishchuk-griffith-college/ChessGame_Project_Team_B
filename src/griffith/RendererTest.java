7835dba163dc11f8819b83a42f03b5e0054ad74e
Mythmaster1x <arshsharma2404@gmail.com>
2025-04-01 18:16:49 +0100

    Adding Test file for Renderer.java

---
+++ src/griffith/RendererTest.java
@@ -0,0 +1,14 @@
+package griffith;
+
+import static org.junit.jupiter.api.Assertions.*; 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class RendererTest {
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+ 
+}
