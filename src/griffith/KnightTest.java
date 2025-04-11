0c3c394d75bf894c439379bf6374dec278fe3dee
Roman Polishchuk <romanpolishchuk2006@gmail.com>
2025-04-11 21:38:29 +0100

    KnightTest add check for move out of bounds of the board and some commits

--- src/griffith/KnightTest.java
+++ src/griffith/KnightTest.java
@@ -9,17 +9,24 @@ import org.junit.jupiter.api.Test;
 public class KnightTest {
 	@Test
 	void getValidMovesTest() {
+		//Create test board
         Board board = new Board();
+        //Create test piece
         Knight knight = new Knight(board);
 
+        //Add piece in the middle of the board
         board.setPiece(3, 3, knight);
+        //Get all valid moves
         String validMoves = knight.getValidMoves();
+        //Correct valid moves
         String correctValidMoves = "1,4 2,5 4,5 5,4 5,2 4,1 2,1 1,2";
+        //Validate moves
         assertEquals(correctValidMoves, validMoves);
 
         Board board2 = new Board();
         Knight knight2 = new Knight(board2);
         
+        //Test piece in the corner
         board2.setPiece(0, 0, knight2);
         validMoves = knight2.getValidMoves();
         correctValidMoves = "1,2 2,1";
@@ -33,13 +40,23 @@ public class KnightTest {
 
         board.setPiece(3, 3, knight);
 
+        //Valid moves
         assertTrue(knight.isMoveValid(1, 4));
         assertTrue(knight.isMoveValid(2, 5));
         assertTrue(knight.isMoveValid(5, 2));
         assertTrue(knight.isMoveValid(1, 2));
 
+        //Invalid moves
         assertFalse(knight.isMoveValid(6, 6));
         assertFalse(knight.isMoveValid(0, 0));
         assertFalse(knight.isMoveValid(3, 3));
+        
+        Board board2 = new Board();
+        Knight knight2 = new Knight(board2);
+
+        board.setPiece(0, 0, knight2);
+        //Go out of bounds out the board
+        assertFalse(knight.isMoveValid(-1, 2));
+        assertFalse(knight.isMoveValid(-1, -2));
 	}
 }
