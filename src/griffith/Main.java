5b12ac165e9c97e379026fcffb917835802ef87c
Stanislav Kril <kril.stas22@gmail.com>
2025-04-12 13:40:20 +0000

    Update (Main.java) <Comments for easier redability>

--- src/griffith/Main.java
+++ src/griffith/Main.java
@@ -5,9 +5,9 @@ import java.awt.Component;
 
 public class Main {
 	public static void main(String[] args) {
-
+		// Create a new game
 		Game game = new Game("Super Chess", 1080, 720);
-		
+		// Run the game
 		game.Run();
 
 	}
