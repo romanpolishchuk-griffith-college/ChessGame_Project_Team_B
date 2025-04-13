614016268589f099e2d7c89eeba3c02937f96304
Stanislav Kril <kril.stas22@gmail.com>
2025-04-13 17:57:23 +0000

    Update (Rook.java) < Refactor constructor to include piece name and improved formatting in getValidMoves method. >

--- src/griffith/Rook.java
+++ src/griffith/Rook.java
@@ -2,24 +2,29 @@ package griffith;
 
 // Represents a rook in the game.
 public class Rook extends ChessPiece {
-	// Constructor with specified board and color.
-    public Rook(Board board, boolean isWhite) {
-		super(board,isWhite);
-	        
-	    // Load the appropriate knight image
-	    String imageName = isWhite ? "w - rook.png" : "b - rook.png";
-	    loadImage(imageName);
-	  
-	}
-	// Constructor with specified board.
-    public Rook(Board board) {
-  		super(board);  	   
-  	}
-	// Returns the valid moves for the rook.
 
-	@Override
-	public String getValidMoves() {
-		String moves = "";
+    // Constructor with specified board and color.
+    public Rook(Board board, boolean isWhite) {
+
+        super(board, isWhite, "Rook");
+        this.isWhite = isWhite;
+
+        // Load the appropriate knight image
+        String imageName = isWhite ? "w - rook.png" : "b - rook.png";
+        loadImage(imageName);
+
+    }
+
+    // Constructor with specified board.
+    public Rook(Board board) {
+        super(board);
+    }
+    // Returns the valid moves for the rook.
+
+    @Override
+    public String getValidMoves() {
+
+        String moves = "";
         int rookX = -1;
         int rookY = -1;
 
@@ -37,29 +42,27 @@ public class Rook extends ChessPiece {
         for (int y = rookY + 1; y < board.getBoard().length; y++) {
             if (board.getPiece(rookX, y) == null) {
                 moves += rookX + "," + y + " ";
-            } else if (board.getPiece(rookX, y).isWhite != isWhite){
-            	moves += rookX + "," + y + " ";
-            	// Stop if can capture a piece
+            } else if (board.getPiece(rookX, y).isWhite != isWhite) {
+                moves += rookX + "," + y + " ";
+                // Stop if can capture a piece
                 break;
-            }
-            else {
-            	// The same color of a piece
-            	break;
+            } else {
+                // The same color of a piece
+                break;
             }
         }
 
         // Move Down
         for (int y = rookY - 1; y >= 0; y--) {
             if (board.getPiece(rookX, y) == null) {
-            	moves += rookX + "," + y + " ";
-            } else if (board.getPiece(rookX, y).isWhite != isWhite){
-            	moves += rookX + "," + y + " ";
-            	// Stop if can capture a piece
+                moves += rookX + "," + y + " ";
+            } else if (board.getPiece(rookX, y).isWhite != isWhite) {
+                moves += rookX + "," + y + " ";
+                // Stop if can capture a piece
                 break;
-            }
-            else {
-            	// The same color of a piece
-            	break;
+            } else {
+                // The same color of a piece
+                break;
             }
         }
 
@@ -67,41 +70,39 @@ public class Rook extends ChessPiece {
         for (int x = rookX + 1; x < board.getBoard()[0].length; x++) {
             if (board.getPiece(x, rookY) == null) {
                 moves += x + "," + rookY + " ";
-            } else if (board.getPiece(x, rookY).isWhite != isWhite){
-            	moves += x + "," + rookY + " ";
-            	// Stop if can capture a piece
+            } else if (board.getPiece(x, rookY).isWhite != isWhite) {
+                moves += x + "," + rookY + " ";
+                // Stop if can capture a piece
                 break;
-            }
-            else {
-            	// The same color of a piece
-            	break;
+            } else {
+                // The same color of a piece
+                break;
             }
         }
 
         // Move Left
         for (int x = rookX - 1; x >= 0; x--) {
             if (board.getPiece(x, rookY) == null) {
-            	moves += x + "," + rookY + " ";
-            } else if (board.getPiece(x, rookY).isWhite != isWhite){
-            	moves += x + "," + rookY + " ";
-            	// Stop if can capture a piece
+                moves += x + "," + rookY + " ";
+            } else if (board.getPiece(x, rookY).isWhite != isWhite) {
+                moves += x + "," + rookY + " ";
+                // Stop if can capture a piece
+                break;
+            } else {
+                // The same color of a piece
                 break;
             }
-            else {
-            	// The same color of a piece
-            	break;
-            }
         }
-        
-        return moves.trim();
-	}
 
-	@Override
-	public boolean isMoveValid(int x, int y) {
+        return moves.trim();
+    }
+
+    @Override
+    public boolean isMoveValid(int x, int y) {
         String validMoves = getValidMoves();
         String targetMove = x + "," + y;
 
         return validMoves.contains(targetMove);
-	}
+    }
 
 }
