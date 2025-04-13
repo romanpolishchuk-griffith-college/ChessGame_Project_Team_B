8597fe9fab1c6292532b84765808dcb9a6fdef01
Stanislav Kril <kril.stas22@gmail.com>
2025-04-13 17:52:07 +0000

    Update (King.java) < Refactor constructor to include piece name and improved formatting in getValidMoves method. >

--- src/griffith/King.java
+++ src/griffith/King.java
@@ -2,48 +2,53 @@ package griffith;
 
 // Represents a king.
 public class King extends ChessPiece {
-	// Constructor with specified board and color.
-	public King(Board board, boolean isWhite) {
-		 super(board);
-	        this.isWhite = isWhite;
-	        
-	        // Load the appropriate knight image
-	        String imageName = isWhite ? "w - king.png" : "b - king.png";
-	        loadImage(imageName);
-	   
-	}
-	
-	// Constructor with specified board.
-	public King(Board board) {
-		 super(board);
-	 
-	}
 
-	// Returns the valid moves for the king.
-	@Override
-	public String getValidMoves() {
-		// The moves for the king
-		String moves = "";
-		// The x position of the king
+    // Constructor with specified board and color.
+    public King(Board board, boolean isWhite) {
+
+        super(board, isWhite, "King");
+        this.isWhite = isWhite;
+
+        // Load the appropriate knight image
+        String imageName = isWhite ? "w - king.png" : "b - king.png";
+        loadImage(imageName);
+    }
+
+    // Constructor with specified board.
+    public King(Board board) {
+        super(board);
+    }
+
+    // Returns the valid moves for the king.
+    @Override
+    public String getValidMoves() {
+
+        // The moves for the king
+        String moves = "";
+
+        // The x position of the king
         int kingX = -1;
-		// The y position of the king
+
+        // The y position of the king
         int kingY = -1;
-		// Loop through the board
+
+        // Loop through the board
         for (int y = 0; y < board.getBoard().length; y++) {
-			// Loop through the board
+
+            // Loop through the board
             for (int x = 0; x < board.getBoard()[y].length; x++) {
-				// If the piece is the king
+
+                // If the piece is the king
                 if (board.getPiece(x, y) == this) {
-					// Set the x position of the king
-                	kingX = x;
-					// Set the y position of the king
-                	kingY = y;
-					// Break the loop
+
+                    // Set the x position of the king
+                    kingX = x;
+
                     break;
                 }
             }
         }
-        
+
         if (kingX < board.getBoard()[0].length && kingX >= 0
         		&& kingY + 1 < board.getBoard().length && kingY + 1 >= 0 &&
         		(board.getPiece(kingX, kingY + 1) == null ||
@@ -52,7 +57,7 @@ public class King extends ChessPiece {
         		{
             moves += kingX + "," + (kingY + 1) + " ";
         }
-        
+
         if (kingX < board.getBoard()[0].length && kingX >= 0
         		&& kingY - 1 < board.getBoard().length && kingY - 1 >= 0 &&
                 		(board.getPiece(kingX, kingY - 1) == null ||
@@ -60,7 +65,7 @@ public class King extends ChessPiece {
                 		board.getPiece(kingX, kingY - 1).isWhite != isWhite))) {
             moves += kingX + "," + (kingY - 1) + " ";
         }
-        
+
         if (kingX + 1 < board.getBoard()[0].length && kingX + 1 >= 0
         		&& kingY < board.getBoard().length && kingY >= 0 &&
                 		(board.getPiece(kingX + 1, kingY) == null ||
@@ -68,7 +73,7 @@ public class King extends ChessPiece {
                 		board.getPiece(kingX + 1, kingY).isWhite != isWhite))) {
             moves += (kingX + 1) + "," + kingY + " ";
         }
-        
+
         if (kingX - 1 < board.getBoard()[0].length && kingX - 1 >= 0
         		&& kingY < board.getBoard().length && kingY >= 0 &&
                 		(board.getPiece(kingX - 1, kingY) == null ||
@@ -76,7 +81,7 @@ public class King extends ChessPiece {
                 		board.getPiece(kingX - 1, kingY).isWhite != isWhite))) {
             moves += (kingX - 1) + "," + kingY + " ";
         }
-        
+
         if (kingX + 1 < board.getBoard()[0].length && kingX + 1 >= 0
         		&& kingY + 1 < board.getBoard().length && kingY + 1 >= 0 &&
                 		(board.getPiece(kingX + 1, kingY + 1) == null ||
@@ -84,7 +89,7 @@ public class King extends ChessPiece {
                 		board.getPiece(kingX + 1, kingY + 1).isWhite != isWhite))) {
             moves += (kingX + 1) + "," + (kingY + 1) + " ";
         }
-        
+
         if (kingX - 1 < board.getBoard()[0].length && kingX - 1 >= 0
         		&& kingY + 1 < board.getBoard().length && kingY + 1>= 0 &&
                 		(board.getPiece(kingX - 1, kingY + 1) == null ||
@@ -92,7 +97,7 @@ public class King extends ChessPiece {
                 		board.getPiece(kingX - 1, kingY + 1).isWhite != isWhite))) {
             moves += (kingX - 1) + "," + (kingY + 1) + " ";
         }
-        
+
         if (kingX + 1 < board.getBoard()[0].length && kingX + 1 >= 0
         		&& kingY - 1 < board.getBoard().length && kingY - 1 >= 0 &&
                 		(board.getPiece(kingX + 1, kingY - 1) == null ||
@@ -100,7 +105,7 @@ public class King extends ChessPiece {
                 		board.getPiece(kingX + 1, kingY - 1).isWhite != isWhite))) {
             moves += (kingX + 1) + "," + (kingY - 1) + " ";
         }
-        
+
         if (kingX - 1 < board.getBoard()[0].length && kingX - 1 >= 0
         		&& kingY - 1 < board.getBoard().length && kingY - 1 >= 0 &&
                 		(board.getPiece(kingX - 1, kingY - 1) == null ||
@@ -108,18 +113,21 @@ public class King extends ChessPiece {
                 		board.getPiece(kingX - 1, kingY - 1).isWhite != isWhite))) {
             moves += (kingX - 1) + "," + (kingY - 1) + " ";
         }
-        
-        return moves.trim();
-	}
 
-	// Returns true if the move is valid for the king.
-	@Override
-	public boolean isMoveValid(int x, int y) {
-		// Get the valid moves for the king
+        return moves.trim();
+    }
+
+    // Returns true if the move is valid for the king.
+    @Override
+    public boolean isMoveValid(int x, int y) {
+
+        // Get the valid moves for the king
         String validMoves = getValidMoves();
-		// Get the target move
+
+        // Get the target move
         String targetMove = x + "," + y;
-		// Return if the move is valid
+
+        // Return if the move is valid
         return validMoves.contains(targetMove);
-	}
+    }
 }
