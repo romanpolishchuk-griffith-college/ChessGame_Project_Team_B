2203d1abcdf8efc54ac8705f9b7305c618cdd7a4
Stanislav Kril <kril.stas22@gmail.com>
2025-04-27 16:44:04 +0000

    Update (Board.java) < Refactor class for easier readability >

--- src/griffith/Board.java
+++ src/griffith/Board.java
@@ -9,8 +9,8 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JOptionPane;
 
-// Represents the game board for a chess game.
 public class Board extends JPanel {
+
     private static final int BOARD_SIZE = 8;
     private static final int SQUARE_SIZE = 80;
 
@@ -18,10 +18,8 @@ public class Board extends JPanel {
     private java.util.List<ChessPiece> capturedBlackPieces = new java.util.ArrayList<>();
     private boolean gameOver = false;
 
-    private JPanel piecePanel;
     private JFrame window;
 
-    //  private ChessPiece[][] board;
     private ChessPiece[][] board = {
             {null, null, null, null, null, null, null, null},
             {null, null, null, null, null, null, null, null},
@@ -33,7 +31,8 @@ public class Board extends JPanel {
             {null, null, null, null, null, null, null, null}
     };
 
-    // Constructor that initializes the board with pieces if specified.
+
+    // Constructors
     public Board(boolean initializePieces) {
         setPreferredSize(new java.awt.Dimension(BOARD_SIZE * SQUARE_SIZE, BOARD_SIZE * SQUARE_SIZE));
         board = new ChessPiece[BOARD_SIZE][BOARD_SIZE];
@@ -42,86 +41,9 @@ public class Board extends JPanel {
         }
     }
 
-    // Default constructor that creates a full game board.
     public Board() {
     }
 
-    // Initializes the pieces on the board.
-    public void initializePieces() {
-        // Initialize White pieces
-        // Pawns
-        for (int col = 0; col < BOARD_SIZE; col++) {
-            setPiece(col, 1, new Pawn(this, true));
-        }
-        // Rooks
-        setPiece(0, 0, new Rook(this, true));
-        setPiece(7, 0, new Rook(this, true));
-        // Knights
-        setPiece(1, 0, new Knight(this, true));
-        setPiece(6, 0, new Knight(this, true));
-        // Bishops
-        setPiece(2, 0, new Bishop(this, true));
-        setPiece(5, 0, new Bishop(this, true));
-        // Queen
-        setPiece(3, 0, new Queen(this, true));
-        // King
-        setPiece(4, 0, new King(this, true));
-
-        //   Initialize Black pieces
-        //   pawns
-        for (int col = 0; col < BOARD_SIZE; col++) {
-            setPiece(col, 6, new Pawn(this, false));
-        }
-        // Rooks
-        setPiece(0, 7, new Rook(this, false));
-        setPiece(7, 7, new Rook(this, false));
-        // Knights
-        setPiece(1, 7, new Knight(this, false));
-        setPiece(6, 7, new Knight(this, false));
-        // Bishops
-        setPiece(2, 7, new Bishop(this, false));
-        setPiece(5, 7, new Bishop(this, false));
-        // Queen
-        setPiece(3, 7, new Queen(this, false));
-        // King
-        setPiece(4, 7, new King(this, false));
-    }
-
-    // Returns the current board configuration.
-    public ChessPiece[][] getBoard() {
-        return board;
-    }
-
-    // Sets the board configuration to the specified array.
-    public void setBoard(ChessPiece[][] board) {
-        this.board = board;
-    }
-
-    // Returns the size of the board.
-    public int getBoredSize() {
-        return BOARD_SIZE;
-    }
-
-    // Returns the piece at the specified coordinates.
-    public ChessPiece getPiece(int x, int y) {
-        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length) {
-            return null;
-        }
-        return board[board.length - 1 - y][x];
-    }
-
-    // Sets the piece at the specified coordinates.
-    public void setPiece(int x, int y, ChessPiece piece) {
-        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length) {
-            return;
-        }
-        board[board.length - 1 - y][x] = piece;
-    }
-
-    public boolean isGameOver() {
-        return gameOver;
-    }
-    
     public void movePiece(ChessPiece piece, int newX, int newY) {
         int pieceX = -1;
         int pieceY = -1;
@@ -146,7 +68,7 @@ public class Board extends JPanel {
             addCapturedPiece(targetPiece);
             //Verifying game window is not null
             if (window != null) {
-                //Removing that chesss piece from the game window
+                //Removing that chess piece from the game window
                 window.remove(targetPiece.button);
             }
         }
@@ -162,6 +84,45 @@ public class Board extends JPanel {
         setPiece(pieceX, pieceY, null);
         //Move piece to new place
         setPiece(newX, newY, piece);
+    }
+
+    // Initializes the pieces on the board.
+    public void initializePieces() {
+        // White pieces
+        for (int col = 0; col < BOARD_SIZE; col++) {
+            setPiece(col, 1, new Pawn(this, true));
+        }
+
+        setPiece(0, 0, new Rook(this, true));
+        setPiece(7, 0, new Rook(this, true));
+
+        setPiece(1, 0, new Knight(this, true));
+        setPiece(6, 0, new Knight(this, true));
+
+        setPiece(2, 0, new Bishop(this, true));
+        setPiece(5, 0, new Bishop(this, true));
+
+        setPiece(3, 0, new Queen(this, true));
+
+        setPiece(4, 0, new King(this, true));
+
+        //   Black pieces
+        for (int col = 0; col < BOARD_SIZE; col++) {
+            setPiece(col, 6, new Pawn(this, false));
+        }
+
+        setPiece(0, 7, new Rook(this, false));
+        setPiece(7, 7, new Rook(this, false));
+
+        setPiece(1, 7, new Knight(this, false));
+        setPiece(6, 7, new Knight(this, false));
+
+        setPiece(2, 7, new Bishop(this, false));
+        setPiece(5, 7, new Bishop(this, false));
+
+        setPiece(3, 7, new Queen(this, false));
+
+        setPiece(4, 7, new King(this, false));
     }
 
     // Draws the board and its pieces.
@@ -187,22 +148,6 @@ public class Board extends JPanel {
 
     }
 
-    // Returns the color of the square at the specified coordinates.
-    public Color getSquareColor(int row, int column) {
-        // Check if the sum of the row and column is even
-        if ((row + column) % 2 == 0) {
-            return Color.WHITE;
-        } else {
-            return Color.BLACK;
-        }
-
-    }
-
-    // Returns the size of the square on the board.
-    public int getSquareSize() {
-        return SQUARE_SIZE;
-    }
-
     //Method to manage captured pieces
     public void addCapturedPiece(ChessPiece piece) {
         if (piece.isWhite) {
@@ -212,14 +157,8 @@ public class Board extends JPanel {
         }
     }
 
-    public java.util.List<ChessPiece> getCapturedWhitePieces() {
-        return capturedWhitePieces;
-    }
 
-    public java.util.List<ChessPiece> getCapturedBlackPieces() {
-        return capturedBlackPieces;
-    }
-
+    // Boolean flags
     public ChessPiece isSquareUnderAttack(int squareX, int squareY, boolean isEnemyWhite) {
         for (int y = 0; y < BOARD_SIZE; y++) {
             for (int x = 0; x < BOARD_SIZE; x++) {
@@ -307,11 +246,11 @@ public class Board extends JPanel {
         int attackingPieceX = attackingPiece.getX();
         int attackingPieceY = attackingPiece.getY();
 
-        //String[] validMovesOFAttakingPiece = (attackingPiece.getValidMoves() + " " + attackingPieceX + "," + attackingPieceY).split(" ");
-        String[] validMovesOfAttakingPiece = (attackingPieceX + "," + attackingPieceY).split(" ");
+        //String[] validMovesOFAttackingPiece = (attackingPiece.getValidMoves() + " " + attackingPieceX + "," + attackingPieceY).split(" ");
+        String[] validMovesOfAttackingPiece = (attackingPieceX + "," + attackingPieceY).split(" ");
         
-        for (int i = 0; i < validMovesOfAttakingPiece.length; i++) {
-            if (isSquareUnderAttack(Integer.parseInt(validMovesOfAttakingPiece[i].charAt(0) + ""), Integer.parseInt(validMovesOfAttakingPiece[i].charAt(2) + ""), false) != null) {
+        for (int i = 0; i < validMovesOfAttackingPiece.length; i++) {
+            if (isSquareUnderAttack(Integer.parseInt(validMovesOfAttackingPiece[i].charAt(0) + ""), Integer.parseInt(validMovesOfAttackingPiece[i].charAt(2) + ""), false) != null) {
                 return false;
             }
         }
@@ -394,16 +333,20 @@ public class Board extends JPanel {
         int attackingPieceX = attackingPiece.getX();
         int attackingPieceY = attackingPiece.getY();
 
-        //String[] validMovesOFAttakingPiece = (attackingPiece.getValidMoves() + " " + attackingPieceX + "," + attackingPieceY).split(" ");
-        String[] validMovesOfAttakingPiece = (attackingPieceX + "," + attackingPieceY).split(" ");
+        //String[] validMovesOFAttackingPiece = (attackingPiece.getValidMoves() + " " + attackingPieceX + "," + attackingPieceY).split(" ");
+        String[] validMovesOfAttackingPiece = (attackingPieceX + "," + attackingPieceY).split(" ");
         
-        for (int i = 0; i < validMovesOfAttakingPiece.length; i++) {
-            if (isSquareUnderAttack(Integer.parseInt(validMovesOfAttakingPiece[i].charAt(0) + ""), Integer.parseInt(validMovesOfAttakingPiece[i].charAt(2) + ""), true) != null) {
+        for (int i = 0; i < validMovesOfAttackingPiece.length; i++) {
+            if (isSquareUnderAttack(Integer.parseInt(validMovesOfAttackingPiece[i].charAt(0) + ""), Integer.parseInt(validMovesOfAttackingPiece[i].charAt(2) + ""), true) != null) {
                 return false;
             }
         }
 
         return true;
+    }
+
+    public boolean isGameOver() {
+        return gameOver;
     }
 
     public boolean isUnderCheck(boolean isCheckForWhite) {
@@ -430,7 +373,22 @@ public class Board extends JPanel {
 
         return false;
     }
-    
+
+
+    // Setters
+    public void setBoard(ChessPiece[][] board) {
+        this.board = board;
+    }
+
+    public void setPiece(int x, int y, ChessPiece piece) {
+        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length) {
+            return;
+        }
+        board[board.length - 1 - y][x] = piece;
+    }
+
+
+    // Getters
     public ArrayList<Map<ChessPiece, String>> getValidMovesUnderCheck(boolean forWhitePlayer){
         ArrayList<Map<ChessPiece, String>> validMoves = new ArrayList<>();
 
@@ -520,19 +478,57 @@ public class Board extends JPanel {
         int attackingPieceX = attackingPiece.getX();
         int attackingPieceY = attackingPiece.getY();
 
-        //String[] validMovesOfAttakingPiece = (attackingPiece.getValidMoves() + attackingPieceX + "," + attackingPieceY).split(" ");
-        String[] validMovesOfAttakingPiece = (attackingPieceX + "," + attackingPieceY).split(" ");
+        //String[] validMovesOfAttackingPiece = (attackingPiece.getValidMoves() + attackingPieceX + "," + attackingPieceY).split(" ");
+        String[] validMovesOfAttackingPiece = (attackingPieceX + "," + attackingPieceY).split(" ");
         
         
-        for (int i = 0; i < validMovesOfAttakingPiece.length; i++) {
-            ChessPiece defendingPiece = isSquareUnderAttack(Integer.parseInt(validMovesOfAttakingPiece[i].charAt(0) + ""), Integer.parseInt(validMovesOfAttakingPiece[i].charAt(2) + ""), forWhitePlayer);
+        for (int i = 0; i < validMovesOfAttackingPiece.length; i++) {
+            ChessPiece defendingPiece = isSquareUnderAttack(Integer.parseInt(validMovesOfAttackingPiece[i].charAt(0) + ""), Integer.parseInt(validMovesOfAttackingPiece[i].charAt(2) + ""), forWhitePlayer);
             if (defendingPiece != null) {
                 Map<ChessPiece, String> move = new HashMap<>();
-                move.put(defendingPiece, validMovesOfAttakingPiece[i].charAt(0) + "," + validMovesOfAttakingPiece[i].charAt(2));
+                move.put(defendingPiece, validMovesOfAttackingPiece[i].charAt(0) + "," + validMovesOfAttackingPiece[i].charAt(2));
                 validMoves.add(move);
             }
         }
 
         return validMoves;
     }
+
+    public Color getSquareColor(int row, int column) {
+        // Check if the sum of the row and column is even
+        if ((row + column) % 2 == 0) {
+            return Color.WHITE;
+        } else {
+            return Color.BLACK;
+        }
+
+    }
+
+    public int getSquareSize() {
+        return SQUARE_SIZE;
+    }
+
+    public ChessPiece[][] getBoard() {
+        return board;
+    }
+
+    public int getBoredSize() {
+        return BOARD_SIZE;
+    }
+
+    public ChessPiece getPiece(int x, int y) {
+        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length) {
+            return null;
+        }
+        return board[board.length - 1 - y][x];
+    }
+
+    public java.util.List<ChessPiece> getCapturedWhitePieces() {
+        return capturedWhitePieces;
+    }
+
+    public java.util.List<ChessPiece> getCapturedBlackPieces() {
+        return capturedBlackPieces;
+    }
+
 }
